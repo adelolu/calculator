b30a123c2e71f05d763d5c5fc54a55fbd7e36703
oyinloye temiloluwa <adelolu6@gmail.com>
2020-12-15 21:10:14 +0100

    i rewrote my jscode to be short and added a js ext file

---
+++ calc.js
@@ -0,0 +1,9 @@
+function number(num) {
+    document.getElementById('main').innerHTML += num;
+}
+function clears(clrs) {
+    document.getElementById('main').innerHTML = clrs;
+}
+function calc(oper) {
+    document.getElementById('main').innerHTML += oper;
+}
